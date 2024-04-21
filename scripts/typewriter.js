038bd4f63294b83d3f954a3e494e5b7d0b2f9faa
swagm0nster <98782228+swagm0nster@users.noreply.github.com>
2024-04-21 17:35:08 -0400

    word dump updates

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -1,6 +1,6 @@
 function typeWriterEffect() {
 
-  const words = [ 'IRON', 'CORAL', 'BLOOD', 'SNOW', 'LIGHT', 'WINE', 'FIRE'];
+  const words = [ "LET THE EMBER BREATHE", "A LOST LETTER", "CLASP ONTO THE CORAL", "A PORCELAIN PARADISE", "DON'T SIFT THE SAND", "A WORRIED WARRIOR", "FEEL THE TREE RINGS", "A CONFUSED COMET"];
   let wordCount = 0;
   let letterCount = 0;
 
@@ -29,7 +29,7 @@ function typeWriterEffect() {
     timeOut = isDeleting ? 100 : 100;
 
     if(!isDeleting && currentText.length === currentWord.length) {
-      timeOut = 3600;
+      timeOut = 1500;
       isDeleting = true;
     } else if(isDeleting && currentText.length === 0) {
       timeOut = 100;
